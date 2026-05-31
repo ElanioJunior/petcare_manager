5e3863ce5da6df88856b54940f50121614d19a8a
elanio <elaniodias44@gmail.com>
2026-05-30 22:16:56 -0300

    criei o construtor de servico

--- src/br/ufc/quixada/petcare/model/Servico.java
+++ src/br/ufc/quixada/petcare/model/Servico.java
@@ -1,5 +1,12 @@
 package br.ufc.quixada.petcare.model;
 
 public abstract class Servico {
+    private String data;
+    private String descricao;
+
+    public Servico(String data, String descricao){
+        this.data=data;
+        this.descricao=descricao;
+    }
 
 }
