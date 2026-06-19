e4a16ef21eaac1cc0b9da767149f8892046c8bfa
elanio <elaniodias44@gmail.com>
2026-06-19 01:46:08 -0300

    atualizado certo

--- src/br/ufc/quixada/petcare/model/Atendimento.java
+++ src/br/ufc/quixada/petcare/model/Atendimento.java
@@ -1,4 +1,43 @@
 package br.ufc.quixada.petcare.model;
 
-public class Atendimento {
+import br.ufc.quixada.petcare.exception.HorarioInvalidoException;
+import br.ufc.quixada.petcare.interfaces.Agendavel;
+
+public class Atendimento implements Agendavel {
+    private Tutor tutor;
+    private Pet pet;
+    private Servico servico;
+    private String horario;
+
+    public Atendimento(Tutor tutor, Pet pet, Servico servico, String horario){
+        if (!horario.matches("\\d{2}:\\d{2}")){
+            throw new HorarioInvalidoException("Horario invalido");
+        }
+        this.tutor = tutor;
+        this.pet = pet;
+        this.servico = servico;
+        this.horario = horario;
+    }
+
+    @Override
+    public boolean agendar(String data, String hora) {
+        return true;
+    }
+
+    @Override
+    public boolean cancelar() {
+        return true;
+    }
+
+    public String gerarResumo(){
+        return "Tutor: " + tutor.getNome() +
+                "\nPet: " + pet.getNome() +
+                "\nServiço: " + servico.getDescricao() +
+                "\nValor: R$" + servico.calcularValor();
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
 }
--- src/br/ufc/quixada/petcare/model/Tutor.java
+++ src/br/ufc/quixada/petcare/model/Tutor.java
@@ -1,4 +1,37 @@
 package br.ufc.quixada.petcare.model;
 
+import br.ufc.quixada.petcare.exception.CPFInvalidoException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tutor {
+    private String nome;
+    private String cpf;
+    private List<Pet> pets;
+
+    public Tutor(String nome, String cpf){
+        if (cpf.length() != 11){
+            throw new CPFInvalidoException("CPF invalido");
+        }
+        this.pets = new ArrayList<>();
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public void adicionarPet(Pet pet){
+        pets.add(pet);
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
 }
