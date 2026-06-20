271c87eaa3d6012c7ff70865277d151615eb3346
elanio <elaniodias44@gmail.com>
2026-06-20 20:42:58 -0300

    criação da exception HorarioOcupadoException dentro do AtendimentoTest.java,e tambem a implementação do before each no teste unitario, para evitar falhas

--- src/br/ufc/quixada/petcare/testes/AtendimentoTest.java
+++ src/br/ufc/quixada/petcare/testes/AtendimentoTest.java
@@ -1,12 +1,23 @@
 package br.ufc.quixada.petcare.testes;
 
 import br.ufc.quixada.petcare.exception.HorarioInvalidoException;
+import br.ufc.quixada.petcare.exception.HorarioOcupadoException;
 import br.ufc.quixada.petcare.model.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import br.ufc.quixada.petcare.exception.ValidacaoException;
+import org.junit.jupiter.api.BeforeEach;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 public class AtendimentoTest {
+
+    @BeforeEach
+    void limparHorarios() throws Exception{
+        Field field = Atendimento.class.getDeclaredField("horariosOcupados");
+        field.setAccessible(true);
+        ((java.util.List<?>) field.get(null)).clear();
+    }
 
     @Test
     void AtendimentoDadosValidos(){
@@ -37,6 +48,18 @@ public class AtendimentoTest {
             ProcedimentoEstetico servico = new ProcedimentoEstetico("20/06/2026", "serviço basico, cliente regula a tosa a cada 3 semanas", "tosa");
 
             Atendimento atendimento =  new Atendimento(tutor, pet, servico, "abc");
+        });
+    }
+
+    @Test
+    void HorarioOcupadoException(){
+        Tutor tutor = new Tutor("fernando", "12345678912");
+        Pet pet = new Pet("toby", "cachorro", 10.0);
+        ProcedimentoEstetico servico = new ProcedimentoEstetico("20/06/2026", "serviço basico, cliente regula a tosa a cada 3 semanas", "tosa");
+
+        assertThrows(HorarioOcupadoException.class, () -> {
+            Atendimento atendimento1 =  new Atendimento(tutor, pet, servico, "19:30");
+            Atendimento atendimento2 =  new Atendimento(tutor, pet, servico, "19:30");
         });
     }
 }
